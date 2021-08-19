65869003d01b99cf3a5202deea7739ce5fe85704
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2021-08-19 13:12:21 +0600

    dd

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/telegram-bot' , 'BotController@index')->name('bot.index');
+Route::any('/telegram-bot' , 'BotController@index')->name('bot.index');
 
